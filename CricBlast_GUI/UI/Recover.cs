aa3d204b189974d205abaf07c639079f7f8b18bc
Raofin <71943580+Raofin@users.noreply.github.com>
2021-12-26 22:12:22 +0600

    Update recover feature

--- CricBlast_GUI/UI/Recover.cs
+++ CricBlast_GUI/UI/Recover.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Media;
 using System.Windows.Forms;
 using CricBlast_GUI.Database;
@@ -27,22 +28,34 @@ namespace CricBlast_GUI.UI
             InitializeComponent();
         }
 
+        private bool _recovered;
+
         private void recoverButton_Click(object sender, EventArgs e)
         {
+            error.Visible = string.IsNullOrWhiteSpace(emailTextBox.Text);
+
+            if (_recovered)
+            {
+                Close();
+                return;
+            }
+
             if (Login.Recover(emailTextBox.Text))
             {
                 recoveredName.Text = Selected.UserDetails[1];
                 recoveredPassword.Text = Selected.UserDetails[3];
                 recoveredUserPicture.Image = Selected.UserImage;
+                recoverButton.FillColor = Color.FromArgb(37, 161, 92);
+                recoverButton.Text = "OK";
                 SystemSounds.Asterisk.Play();
+                _recovered = true;
             }
-
-            error.Visible = string.IsNullOrWhiteSpace(emailTextBox.Text);
+            else new MessageBoxOk(2, "There are no accounts linked to that email address.").ShowDialog();
         }
 
         private void emailTextBox_TextChanged(object sender, EventArgs e)
         {
-            error.Visible = false;
+            error.Visible = string.IsNullOrWhiteSpace(emailTextBox.Text);
         }
     }
 }
