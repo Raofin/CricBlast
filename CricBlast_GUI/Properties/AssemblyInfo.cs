985cbaf351bb2f2544f99066c9b4c80f31f441ca
Raofin <71943580+Raofin@users.noreply.github.com>
2022-01-05 21:33:05 +0600

    Fix assembly info

--- CricBlast_GUI/Properties/AssemblyInfo.cs
+++ CricBlast_GUI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CricBlast_GUI")]
+[assembly: AssemblyTitle("CricBlast")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CricBlast_GUI")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("CricBlast")]
+[assembly: AssemblyCopyright("Copyright © 2021 Raofin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
